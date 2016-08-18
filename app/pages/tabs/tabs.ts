e2f4ce7bf377f4f278caa7b6a836570b93ca4e0f
Mariano Alvarez <marianoa@thehangar.cr>
2016-08-17 18:27:48 -0600

    beta.11 - add settings tab and include log out logic

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,7 +1,11 @@
-import {Component} from '@angular/core';
-import {HomePage} from '../home/home';
-import {AboutPage} from '../about/about';
-import {ContactPage} from '../contact/contact';
+import { Component }              from '@angular/core';
+import { Events, NavController }  from 'ionic-angular';
+
+import { LoginPage }    from '../initialsetup/login/login';
+import { HomePage }     from '../home/home';
+import { AboutPage }    from '../about/about';
+import { SettingsPage } from '../settings/settings';
+
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -12,11 +16,20 @@ export class TabsPage {
   private tab2Root: any;
   private tab3Root: any;
 
-  constructor() {
-    // this tells the tabs component which Pages
-    // should be each tab's root Page
+  constructor(private events:Events, private nav:NavController) {
     this.tab1Root = HomePage;
     this.tab2Root = AboutPage;
-    this.tab3Root = ContactPage;
+    this.tab3Root = SettingsPage;
   }
+
+  ngAfterViewInit() {
+    let animation = {
+          animate: true,
+          direction: 'back'
+        };
+    this.events.subscribe('event:logout', ()=>{
+      this.nav.setRoot(LoginPage, null, animation);
+    });
+  }
+
 }
