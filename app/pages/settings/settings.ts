0d8e5bd326c985e7a5f51c97f71779d5014ebad2
Mariano Alvarez <marianoa@thehangar.cr>
2016-08-17 18:28:09 -0600

    beta.11 - create log out logic in settngs

--- app/pages/settings/settings.ts
+++ app/pages/settings/settings.ts
@@ -1,38 +1,28 @@
-import {IonicApp, Page, NavController} from 'ionic-angular';
-import {ProfilePage} from './profile/profile';
-import {BlankPage} from '../blank/blank';
-import {DevicePage} from '../device/device';
-import {PushNotificationsService} from '../../global/services/push_notifications';
+import { Component }     from '@angular/core';
 
-@Page({
-  templateUrl: 'build/pages/settings/settings.html',
-  providers: [PushNotificationsService]
+import { NavController, Events } from 'ionic-angular';
+import { DevicePage }            from '../device/device';
+
+@Component({
+  templateUrl: 'build/pages/settings/settings.html'
 })
 export class SettingsPage {
+
   private loading: any;
-  constructor(private nav: NavController, private app: IonicApp, private pushNotifications: PushNotificationsService) {
+
+  constructor(private events:Events, private nav: NavController) {
     this.nav = nav;
-    this.loading = this.app.getComponent('loading');
   }
-  openProfile() {
-    this.nav.push(ProfilePage);
-  }
-  openPage() {
-    this.nav.push(BlankPage);
-  }
+
   openDevicePage() {
     this.nav.push(DevicePage);
   }
-  showLoader() {
-    this.loading.show();
-    setTimeout(() => {
-        this.loading.hide();
-    }, 2000);
-  }
-  showOneSignalId() {
-    this.pushNotifications.getIds(this.showUserId);
-  }
+
   showUserId(ids) {
     alert(ids.userId);
+  }
+
+  logout() {
+    this.events.publish('event:logout');
   }
 }
